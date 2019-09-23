f4381df95800aba53d5a91e6cdb7bfd233cf7cce
RawBData <lunarmonky@yahoo.com>
2019-09-23 13:11:37 -0700

    Setting up frontend

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>Something is Twittering</p>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
