e0084c87f47793aacdc6f50aae77a02baab9f67b
kmoch <kevin.edholm.moch@gmail.com>
2019-09-23 22:24:16 -0700

    finished backend user auth and jwt

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require('passport');
 const app = express();
+const db = require('./config/keys').mongoURI;
 
-app.get("/", (req, res) => res.send("Hello World"));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+// require("./models/User")
+const users = require("./routes/api/users");
+const gameStats = require("./routes/api/gameStats");
+
+app.use("/api/users", users);
+app.use("/api/gameStats", gameStats);
 
 const port = process.env.port || 5000;
 
