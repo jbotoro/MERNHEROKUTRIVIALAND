40ed8543557eb68bff7f43a0e03eaa7adfab8966
RawBData <lunarmonky@yahoo.com>
2019-09-26 09:51:38 -0700

    fix action

--- frontend/src/actions/questions_actions.js
+++ frontend/src/actions/questions_actions.js
@@ -1,10 +1,11 @@
 import * as questionUtil from '../util/questions_util';
 export const RECEIVE_ROUND_1_QUESTIONS = "RECEIVE_ROUND_1_QUESTIONS";
 
-const receiveRound1Questions = (questions) => {
+const receiveRound1Questions = (questions) => ({
     type: RECEIVE_ROUND_1_QUESTIONS,
     questions
-};
+
+})
 
 export const round1Questions = () => dispatch => questionUtil.round1Questions()
     .then( questions => dispatch(receiveRound1Questions(questions)));
