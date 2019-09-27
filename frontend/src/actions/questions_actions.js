21cda150e5a6897449e60c0ec5a39fcac8b3d8c7
ashap94 <aaronshapiro94@gmail.com>
2019-09-26 18:32:31 -0700

    Modified reducer to format questions into pojo that sorts 5 questions for each category

--- frontend/src/actions/questions_actions.js
+++ frontend/src/actions/questions_actions.js
@@ -1,10 +1,12 @@
 import * as questionUtil from '../util/questions_util';
 export const RECEIVE_ROUND_1_QUESTIONS = "RECEIVE_ROUND_1_QUESTIONS";
 
+// debugger
 const receiveRound1Questions = (questions) => ({
     type: RECEIVE_ROUND_1_QUESTIONS,
-    questions
-
+    questions: questions.data,
+    body: ""
+    
 })
 
 export const round1Questions = () => dispatch => questionUtil.round1Questions()
--- frontend/src/reducers/questions_reducer.js
+++ frontend/src/reducers/questions_reducer.js
@@ -1,13 +1,13 @@
 import {
     RECEIVE_ROUND_1_QUESTIONS
 } from '../actions/questions_actions';
-import generateRound1Board from '../../../game_logic/board_1';
+import generateRound1Board from '../game_logic/board_1';
 
 export default (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_ROUND_1_QUESTIONS:
-            let board_1 = generateRound1Board(action.questions);
+            let board_1 = generateRound1Board(action.questions)[0];
             return board_1;
         default:
             return state;
