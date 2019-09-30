1d88d7c42111ebda95c03b2ffc731ce0076867e8
kmoch <kevin.edholm.moch@gmail.com>
2019-09-30 15:28:33 -0700

    another update for heroku

--- app.js
+++ app.js
@@ -31,6 +31,15 @@ mongoose
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
+const port = process.env.port || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 const users = require("./routes/api/users");
 const games = require("./routes/api/games");
 const gameStats = require("./routes/api/gameStats");
@@ -41,17 +50,11 @@ app.use("/api/games", games);
 app.use("/api/gameStats", gameStats);
 app.use("/api/questions", questions);
 
-const port = process.env.port || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
 
+
+/*
 //Wesocket Details Below
 const io = socketIO(server);
 
@@ -85,8 +88,9 @@ io.on('connection', socket => {
   //   // console.log('user disconnected');
   //   socket.leave(socket.room);
   // });
-
+  
 })
+*/
 
 
 // server.listen(port);
