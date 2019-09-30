5d48efa87f30a1382b77e31be296a97336006600
kmoch <kevin.edholm.moch@gmail.com>
2019-09-30 12:41:45 -0700

    key files change before heroku deploy

---
+++ frontend/src/components/game/final_scores/finalScoreDisplayContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import FinalScoreDisplay from './finalScoreDisplay';
+
+const mapStateToProps = state => {
+
+  return {
+
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+
+  return {
+
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FinalScoreDisplay);
