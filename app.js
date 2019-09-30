2282bcad7c6328a0d990d9c4fa741199a48b0b3d
kmoch <kevin.edholm.moch@gmail.com>
2019-09-30 15:48:56 -0700

    changed app js again

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
-// const path = require('path');
-const mongoose = require("mongoose");
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const db = require('./config/keys').mongoURI;
-
-//Express app for routes
+const path = require('path');
 const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
+/*
 //sockets
 const path = require('path');
 const socketIO = require('socket.io');
@@ -19,19 +20,23 @@ const server = http.createServer(app);
 const GameState = require('./models/Game');
 
 //end of socket variables
+*/
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const users = require("./routes/api/users");
+const games = require("./routes/api/games");
+const gameStats = require("./routes/api/gameStats");
+const questions = require("./routes/api/questions");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(passport.initialize());
-require('./config/passport')(passport);
-
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
@@ -40,18 +45,10 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const users = require("./routes/api/users");
-const games = require("./routes/api/games");
-const gameStats = require("./routes/api/gameStats");
-const questions = require("./routes/api/questions");
-
 app.use("/api/users", users);
 app.use("/api/games", games);
 app.use("/api/gameStats", gameStats);
 app.use("/api/questions", questions);
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
 
 
 /*
